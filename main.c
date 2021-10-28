3e5786a02f67a4223b06adf183097c73c84701fe
Logan Ruzzier <lruzzier@DESKTOP-MQGMU3Q.localdomain>
2021-10-27 23:29:31 -0400

    finished homework

--- main.c
+++ main.c
@@ -7,6 +7,23 @@
 #include <string.h>
 
 int main(){
-  printf("%d\n",random_gen());
+  int arr[10];
+  for(int i=0;i<10;i++){
+    arr[i]=random_gen();
+  }
+  for(int j=0;j<10;j++){
+    printf("\trandom %d: %d\n",j,arr[j]);
+  }
+  int h=open("holder1.txt", O_RDONLY | O_WRONLY, 0644);
+  if(h<0){return -10;}
+  write(h, arr, 40);
+  int arr2[10];
+  h=open("holder1.txt", O_RDONLY, 0644);
+  if(h<0){return -10;}
+  read(h,arr2,40);
+  printf("-------\n");
+  for(int k=0;k<10;k++){
+    printf("\tcopied value %d: %d\n",k,arr2[k]);
+  }
   return 0;
 }
